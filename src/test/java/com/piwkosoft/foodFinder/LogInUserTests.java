da127f12b689b73ca273332fac609d4059f47da9
Patryk Piwko <ppiwko@si-ecommerce.pl>
2020-03-27 22:36:39 +0100

    login tests

--- src/test/java/com/piwkosoft/foodFinder/LogInUserTests.java
+++ src/test/java/com/piwkosoft/foodFinder/LogInUserTests.java
@@ -1,19 +1,19 @@
 package com.piwkosoft.foodFinder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.piwkosoft.foodFinder.Controllers.LoginController;
 import com.piwkosoft.foodFinder.Forms.UserLoginForm;
-import com.piwkosoft.foodFinder.Persistance.Entities.AccountPlanEntity;
-import com.piwkosoft.foodFinder.Persistance.Entities.AccountPlanEntity.AccountPlan;
-import com.piwkosoft.foodFinder.Persistance.Entities.UserEntity;
 import com.piwkosoft.foodFinder.Services.UserDetailsServiceImpl;
-import java.util.Date;
+import java.net.UnknownServiceException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,8 +22,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -43,7 +46,8 @@ import org.springframework.web.context.WebApplicationContext;
 @WebMvcTest(value = {LoginController.class})
 public class LogInUserTests {
 
-  public final UserEntity userEntity = new UserEntity();
+  final UserLoginForm userLoginFormWithoutPassword = new UserLoginForm();
+  final UserLoginForm userLoginFormCorrect = new UserLoginForm();
 
   public MockMvc mockMvc;
 
@@ -54,29 +58,62 @@ public class LogInUserTests {
   private WebApplicationContext webApplicationContext;
 
   @BeforeEach
+  public void initWrongUserForm() {
+    this.userLoginFormWithoutPassword.setName("admin@admin.pl");
+  }
+
+  @BeforeEach
+  public void initCorrectUserForm() {
+    this.userLoginFormCorrect.setName("user@user.pl");
+    this.userLoginFormCorrect.setPassword("user1");
+  }
+
+
+  @BeforeEach
   public void initMockMvc() {
-    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    this.mockMvc = MockMvcBuilders
+        .webAppContextSetup(webApplicationContext)
+        .apply(springSecurity())
+        .build();
   }
 
   @Test
-  @DisplayName("Show login page - STATUS OK")
+  @DisplayName("STATUS OK - Show login page")
   public void showLoginPage() throws Exception {
     mockMvc.perform(get("/login")).andExpect(status().isOk());
   }
 
   @Test
-  @DisplayName("logIn test")
+  @DisplayName("STATUS OK - logIn test")
   public void loginTest() throws Exception {
-    final UserLoginForm userLoginForm = new UserLoginForm();
-    userLoginForm.setName("admin");
-    userLoginForm.setPassword("admin");
-
     final ObjectMapper objectMapper = new ObjectMapper();
 
-    mockMvc.perform(post("/login", userLoginForm)
-    .contentType(MediaType.TEXT_HTML)
-    .content(objectMapper.writeValueAsBytes(userLoginForm)))
-    .andExpect(status().isOk());
+    mockMvc.perform(post("/login", userLoginFormWithoutPassword)
+        .with(csrf())
+        .contentType(MediaType.TEXT_HTML)
+        .content(objectMapper.writeValueAsBytes(userLoginFormWithoutPassword)))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  @DisplayName("logIn error")
+  public void resultLoginError() throws Exception {
+    mockMvc.perform(
+        formLogin()
+            .user(userLoginFormWithoutPassword.getName()))
+        .andExpect(forwardedUrl("/login/error"));
+  }
+
+  @Test
+  @DisplayName("logIn correct")
+  public void resultLoginCorrect() throws Exception {
+    mockMvc.perform(
+        formLogin()
+            .user(userLoginFormCorrect.getName())
+            .password(userLoginFormCorrect.getPassword()))
+        .andExpect(forwardedUrl("/"));
+
 
   }
+
 }
