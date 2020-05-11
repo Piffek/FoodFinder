56bd3380f926afd662e9964c5a8a4b2e59387265
Patryk Piwko <ppiwko@si-ecommerce.pl>
2020-05-11 20:10:55 +0200

    add api to show places

--- src/main/java/com/piwkosoft/foodFinder/Core/Controllers/HomePageController.java
+++ src/main/java/com/piwkosoft/foodFinder/Core/Controllers/HomePageController.java
@@ -1,5 +1,11 @@
 package com.piwkosoft.foodFinder.Core.Controllers;
 
+import com.piwkosoft.foodFinder.Core.Facades.Interfaces.PlaceFacade;
+import com.piwkosoft.foodFinder.Core.Services.Interfaces.PlaceService;
+import com.piwkosoft.foodFinder.Dto.PlaceDTO;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,8 +13,23 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomePageController {
 
+    private final static String PLACES = "places";
+
+    private final PlaceFacade placeFacade;
+
+    public HomePageController(final PlaceFacade placeFacade) {
+        this.placeFacade = placeFacade;
+    }
+
     @RequestMapping("/")
     public ModelAndView homePage() {
-        return new ModelAndView("layout");
+        final ModelAndView modelAndView = new ModelAndView("layout");
+
+        final List<PlaceDTO> placeDTOS = Optional.ofNullable(placeFacade.findAllPlaces()).orElse(
+            Collections.emptyList());
+
+        modelAndView.addObject(PLACES, placeDTOS);
+
+        return modelAndView;
     }
 }
